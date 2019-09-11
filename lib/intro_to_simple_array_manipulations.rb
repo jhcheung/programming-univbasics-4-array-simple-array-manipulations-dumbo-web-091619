5f1c99a2187eeae71b4e3bf80d1070a71bcee29a
artificial-semicolon-2761 <jimmy.h.cheung@gmail.com>
2019-09-11 20:50:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -35,5 +35,13 @@ def using_uniq(array)
 end
 
 def using_flatten(array)
+  array.flatten 
+end
 
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete(integer)
 end
