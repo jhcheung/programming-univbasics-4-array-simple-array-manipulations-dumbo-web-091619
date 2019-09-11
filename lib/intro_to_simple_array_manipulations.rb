4c9e7da63a2c1f9348d6f480d53a09db0d934f14
artificial-semicolon-2761 <jimmy.h.cheung@gmail.com>
2019-09-11 20:49:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,5 +27,13 @@ def using_concat(array, array2)
 end
 
 def using_insert(array, element)
-  array.insert()
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+
 end
